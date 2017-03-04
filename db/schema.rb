a761e24ecaf6950a650ab5e9a32b69b83db1a82a
CherutoMercy <m.thirty100@gmail.com>
2017-03-04 18:12:16 +0300

    change db schema to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304124521) do
+ActiveRecord::Schema.define(version: 20170304171402) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,25 @@ ActiveRecord::Schema.define(version: 20170304124521) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.index ["type"], name: "index_ckeditor_assets_on_type"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
