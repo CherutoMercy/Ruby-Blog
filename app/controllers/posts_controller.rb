9cb409e7dcf0ab7df014269393cb69f03524a1fb
CherutoMercy <m.thirty100@gmail.com>
2017-03-04 18:09:27 +0300

    add controller classes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,64 +1,74 @@
 class PostsController < ApplicationController
-  before_action :find_post, only: [:edit, :update, :show, :delete]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # Index action to render all posts
+  # GET /posts
+  # GET /posts.json
   def index
     @posts = Post.all
   end
 
-  # New action for creating post
+  # GET /posts/1
+  # GET /posts/1.json
+  def show
+  end
+
+  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # Create action saves the post into database
-  def create
-    @post = Post.new
-    if @post.save(post_params)
-      flash[:notice] = "Successfully created post!"
-      redirect_to post_path(@post)
-    else
-      flash[:alert] = "Error creating new post!"
-      render :new
-    end
-  end
-
-  # Edit action retrives the post and renders the edit page
+  # GET /posts/1/edit
   def edit
   end
 
-  # Update action updates the post with the new information
-  def update
-    if @post.update_attributes(post_params)
-      flash[:notice] = "Successfully updated post!"
-      redirect_to post_path(@posts)
-    else
-      flash[:alert] = "Error updating post!"
-      render :edit
+  # POST /posts
+  # POST /posts.json
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
     end
   end
 
-  # The show action renders the individual post after retrieving the the id
-  def show
+  # PATCH/PUT /posts/1
+  # PATCH/PUT /posts/1.json
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
-  # The destroy action removes the post permanently from the database
+  # DELETE /posts/1
+  # DELETE /posts/1.json
   def destroy
-    if @post.destroy
-      flash[:notice] = "Successfully deleted post!"
-      redirect_to posts_path
-    else
-      flash[:alert] = "Error updating post!"
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
     end
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
 
-  def post_params
-    params.require(:post).permit(:title, :body)
-  end
-
-  def find_post
-    @post = Post.find(params[:id])
-  end
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
